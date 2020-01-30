54fc085e315e31e83263824bba676b80bf1b17c4
Thomas Roberts <robertsorange@gmail.com>
2020-01-29 18:06:25 -0700

    Resetting player on health change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector3 initialPosition;
     public float speedMultiplier = 10f;
     public int health = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newMovementVector = Vector3.zero;
-        newMovementVector.z = Input.GetAxis("Vertical");
-        newMovementVector.x = Input.GetAxis("Horizontal");
+        if(health <= 0) {
+            resetPlayer();
+        }
+        else {
+            Vector3 newMovementVector = Vector3.zero;
+            newMovementVector.z = Input.GetAxis("Vertical");
+            newMovementVector.x = Input.GetAxis("Horizontal");
 
-        rb.AddForce(newMovementVector * speedMultiplier);
+            rb.AddForce(newMovementVector * speedMultiplier);
+        }
     }
 
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Enemy")) {
+            Debug.Log(health);
+            health -= 2;
+        }
+    }
 
+    private void resetPlayer() {
+        rb.velocity = Vector3.zero;
+        transform.position = initialPosition;
+        health = 100;
+    }
 }
