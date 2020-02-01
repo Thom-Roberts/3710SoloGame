f5fc2b08d154cc7eeb0d56b5501c3c2d21940c9f
Thomas Roberts <robertsorange@gmail.com>
2020-02-01 14:06:09 -0700

    Changing color logic works

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    private GameState gameState;
+    private Material matReference;
+    public Color playerTint;
+    public Color enemyTint;
+
+    private void Start() {
+        gameState = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameState>();
+        matReference = GetComponent<Renderer>().material;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        var tag = collision.gameObject.tag;
+        int xPosition = (int)transform.position.x;
+        int zPosition = (int)transform.position.z;
+        Debug.Log("Collision entered: " + collision);
+        if(tag == "Player") {
+            matReference.color = playerTint;
+            gameState.UpdateState(new Tuple<int, int, string>(xPosition, zPosition, "player"));
+        }
+        else if(tag == "Enemy") {
+            matReference.color = enemyTint;
+            gameState.UpdateState(new Tuple<int, int, string>(xPosition, zPosition, "enemy"));
+        }
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    public int maxGameBoardSize = 7;
+    private string[,] gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = new string[maxGameBoardSize, maxGameBoardSize];
+        for(var i = 0; i < 7; ++i) {
+            for(var j = 0; j < 7; ++j) {
+                gameState[i, j] = "none";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckForWin();
+    }
+
+    void CheckForWin() {
+        int playerCount = 0;
+        int opponentCount = 0;
+
+        for(var i = 0; i < maxGameBoardSize; ++i) {
+            for(var j = 0; j < maxGameBoardSize; ++j) {
+                if(gameState[i, j] == "player") {
+                    playerCount++;
+                }
+                else if(gameState[i, j] == "enemy") {
+                    opponentCount++;
+                }
+            }
+        }
+
+        // Must be more than half of the squares. Doing this casts to make sure I round up
+        // See here: https://code-examples.net/en/q/e0e5c
+        int winSqaureNumber = (int)Math.Ceiling((double)maxGameBoardSize * maxGameBoardSize / 2);
+        if(playerCount > winSqaureNumber) {
+            // Player wins!
+        }
+        else if(opponentCount > winSqaureNumber) {
+            // Enemy wins!
+        }
+    }
+
+    // Comes in with [x, y, player/opponent]
+    // Reason it's an array is in the case that someone doesn't move while the other person does
+    public void UpdateState(Tuple<int, int, string> change) {
+        // For now these divisions are hard set because the elements are scaled by 2 in the x & z directions
+        gameState[change.Item1 / 2, change.Item2 / 2] = change.Item3;
+    }
+}
